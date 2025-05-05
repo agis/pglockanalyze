8454b48bd13bcceff6024fd9f361a630011b6a23
Agis Anastasopoulos <a@xz0.org>
2025-05-05 18:31:00 +0300

    Merge analyzer.analyze_many/analyze_one

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -16,25 +16,9 @@ impl Analyzer {
         })
     }
 
-    pub fn analyze_one(&self, sql: &str) -> Result<Statement, Error> {
-        let mut client = self.config.connect(postgres::NoTls)?;
-        let mut tx = client.transaction()?;
-        let pid = tx.query_one("SELECT pg_backend_pid()", &[])?.get(0);
+    pub fn analyze(&self, sql: &str) -> Result<Vec<Statement>, Error> {
+        const FETCH_PID: &str = "SELECT pg_backend_pid()";
 
-        let statements = pg_query::parse(sql)?.protobuf.stmts;
-        if statements.len() != 1 {
-            return Err("expected a single statement".into());
-        }
-
-        let stmt = statements[0].stmt.as_ref().unwrap().deparse()?;
-        let result = Statement::analyze(&self.config, stmt, &mut tx, pid);
-
-        tx.rollback()?;
-
-        result
-    }
-
-    pub fn analyze_many(&self, sql: &str) -> Result<Vec<Statement>, Error> {
         let stmts = pg_query::parse(sql)?
             .protobuf
             .stmts
@@ -42,16 +26,33 @@ impl Analyzer {
             .map(|stmt| stmt.stmt.unwrap().deparse().unwrap());
 
         if self.wrap_in_transaction {
+            // all statements executed under a single transaction
             let mut client = self.config.connect(postgres::NoTls)?;
             let mut tx = client.transaction()?;
-            let pid = tx.query_one("SELECT pg_backend_pid()", &[])?.get(0);
+            let pid = tx.query_one(FETCH_PID, &[])?.get(0);
+
             let result = stmts
                 .map(|s| Statement::analyze(&self.config, s, &mut tx, pid))
                 .collect();
+
             tx.rollback()?;
+
             result
         } else {
-            stmts.map(|s| self.analyze_one(&s)).collect()
+            // each statement executes in a new transaction
+            let mut result = Vec::new();
+
+            for stmt in stmts {
+                let mut client = self.config.connect(postgres::NoTls)?;
+                let mut tx = client.transaction()?;
+                let pid = tx.query_one(FETCH_PID, &[])?.get(0);
+
+                result.push(Statement::analyze(&self.config, stmt, &mut tx, pid)?);
+
+                tx.rollback()?;
+            }
+
+            Ok(result)
         }
     }
 }
--- src/bin/pglockanalyze/main.rs
+++ src/bin/pglockanalyze/main.rs
@@ -11,7 +11,7 @@ fn main() {
     let input = &cli.input.read_to_string().unwrap_or_else(abort);
 
     let analyzer = Analyzer::new(&cli.db, cli.wrap_in_transaction).unwrap_or_else(abort);
-    let analysis = analyzer.analyze_many(input).unwrap_or_else(abort);
+    let analysis = analyzer.analyze(input).unwrap_or_else(abort);
     let output = cli.formatter.format(analysis);
 
     println!("{output}");
