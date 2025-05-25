0afd66c30f2016601946f1639ebf23a426d4ce04
Agis Anastasopoulos <a@xz0.org>
2025-05-25 15:37:30 +0300

    Expose line no. information for each statement
    This patch adds support for storing the start/end line number for each
    statement present in the original input. The CLI only prints this
    information when the JSON formatter is used, as of now:
    
        [
          {
            "sql": "......",
            "locks_acquired": [],
            "location": {
              "start_line": 1,
              "end_line": 3
            }
        ]
    
    To do this, we migrate from libpq_query to sqlparser, which expose line
    no. information. There's still a small bug reported upstream[1] but this
    is at least a good starting point.
    
    Part of #17
    
    [1] https://github.com/apache/datafusion-sqlparser-rs/issues/1858

--- src/statement.rs
+++ src/statement.rs
@@ -2,23 +2,43 @@ use crate::errors::Error;
 use crate::lock::{Lock, Locks};
 use postgres as pg;
 use serde::{Deserialize, Serialize};
+use sqlparser::ast::Spanned;
+use sqlparser::ast::Statement as AstStatement;
 use std::collections::HashSet;
 use std::fmt;
+
+/// Starting and ending lines in the original input where a statement appears
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Location {
+    pub start_line: u64,
+    pub end_line: u64,
+}
+
+impl From<sqlparser::tokenizer::Span> for Location {
+    fn from(span: sqlparser::tokenizer::Span) -> Self {
+        Self {
+            start_line: span.start.line,
+            end_line: span.end.line,
+        }
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Statement {
     pub sql: String,
     pub locks_acquired: Locks,
+    pub location: Location,
 }
 
 impl Statement {
     pub fn analyze(
         db: &pg::Config,
-        sql: String,
         tx: &mut pg::Transaction,
         pid: i32,
+        stmt: AstStatement,
     ) -> Result<Self, Error> {
         let locks_before = Self::detect_locks(db, pid)?;
+        let sql = stmt.to_string();
         tx.execute(&sql, &[])?;
         let locks_after = Self::detect_locks(db, pid)?;
         let locks_acquired = Locks::compute_acquired(locks_before, locks_after);
@@ -26,6 +46,7 @@ impl Statement {
         Ok(Statement {
             sql,
             locks_acquired,
+            location: stmt.span().into(),
         })
     }
 
